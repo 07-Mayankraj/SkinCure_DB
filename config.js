bc08c673f925c706ce06698242ae63a341e58002
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-12-13 22:37:33 +0530

    server files setup

---
+++ config.js
@@ -0,0 +1,21 @@
+export default {
+  serverPort: 9090,
+  frontendPort: 8080,
+  protectedRoutes: [
+    { route: "/users", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/posts", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/comments", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/photos", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/todos", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/recipeCategories", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/recipeIngredients", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/recipes", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/areas", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/recipeTags", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/orders", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/wishlist", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/cart", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+  ]
+}
+
+// Employees, departments & cats are not protected.
